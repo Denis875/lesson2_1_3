057914a7b2d9d7d839cfe8306f8dfc4be1efc8b9
denis875 <denisok578@gmail.com>
2024-05-15 20:43:31 +0300

    Logger and code style

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,11 +1,14 @@
 package app.model;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
+    private static final Logger logger = LogManager.getLogger(AnimalsCage.class);
 
     @Autowired
     @Qualifier("dog")
@@ -15,11 +18,9 @@ public class AnimalsCage {
     private Timer timer;
 
     public void whatAnimalSay() {
-        System.out.println("Say:");
-        System.out.println(animal.toString());
-        System.out.println("At:");
-        System.out.println(timer.getTime());
-        System.out.println("________________________");
+        logger.info("Say:" + animal.toString() + "At:");
+        logger.info(timer.getTime());
+        logger.info("________________________");
     }
 
     public Timer getTimer() {
